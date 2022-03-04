527be452a51e28845d5b8efb0ddf19fa850d9ced
Trevin Small <trevincub03@gmail.com>
2022-03-03 19:17:55 -0500

    Added for loops!!
    Whatever this new BrainFuck version is, it has shorthand syntax for move and increment/decrement
    EX
    >3 == >>> and +2 == ++
    For loop:
    {x}5 Repeats whatever x is 5 times

--- interpreter_extended.c
+++ interpreter_extended.c
@@ -2,6 +2,7 @@
 
 // Memory size of Brainfuck programs
 #define MEM_SIZE 30000
+#define TEN_KILOBYTE 10000
 
 // All of Brainfucks commands.... only 8 lines LOL
 #define PTR_LEFT '<'
@@ -10,19 +11,68 @@
 #define PTR_DECREMENT '-'
 #define CHAR_IN ','
 #define CHAR_OUT '.'
-#define LOOP_START '['
-#define LOOP_END ']'
+#define BF_LOOP_START '['
+#define BF_LOOP_END ']'
+#define FOR_LOOP_START '{'
+#define FOR_LOOP_END '}'
+
+int for_loop();
+int run(char);
 
 char mem[MEM_SIZE] = {0};
 FILE * fp;
 char * ptr;
 
-int run() {
+int for_loop() {
+  long int for_end_index = ftell(fp);
+  int num_repititions = 0;
+  int num_in = fscanf(fp, "%d", &num_repititions);
+
+  long int size_of_repititions = ftell(fp) - for_end_index;
+
+  if (num_in != 1) {
+    printf("Expected an integer after '{' for loop closing bracket.");
+    return EOF;
+  }
+
+  fseek(fp, for_end_index, SEEK_SET);
+
+  int close_counter = 0;
+
+  do {
+    fseek(fp, -2, SEEK_CUR);
+    char char_in = fgetc(fp);
+    if (char_in == FOR_LOOP_END) {
+      close_counter++;
+    } else if ((char_in == FOR_LOOP_START) && (close_counter > 0)) {
+      close_counter--;
+    } else if ((char_in == FOR_LOOP_START) && (close_counter == 0)) {
+      break;
+    }
+  } while(1 == 1);
+
+  long int for_start_index = ftell(fp);
+
+  for(int i = 1; i < num_repititions; i++) {
+    fseek(fp, for_start_index, SEEK_SET);
+    char next = fgetc(fp);
+    do {
+      run(next);
+      next = fgetc(fp);
+    } while(next != FOR_LOOP_END);
+  }
+
+  fseek(fp, 1 + size_of_repititions, SEEK_CUR);
+
+  return 0;
+}
+
+int run(char command) {
 
   int value = 0;
   int num_in = 0;
 
-  switch(fgetc(fp)) {
+  switch(command) {
 
     case PTR_LEFT:
       --ptr;
@@ -59,16 +109,16 @@ int run() {
       printf("%c", *ptr);
       break;
 
-    case LOOP_START:
+    case BF_LOOP_START:
       if (*ptr == 0) {
         int start_counter = 0;
         do {
           char start_in = fgetc(fp);
-          if (start_in == LOOP_START) {
+          if (start_in == BF_LOOP_START) {
             start_counter++;
-          } else if ((start_in == LOOP_END) && (start_counter > 0)) {
+          } else if ((start_in == BF_LOOP_END) && (start_counter > 0)) {
             start_counter--;
-          } else if ((start_in == LOOP_END) && (start_counter == 0)) {
+          } else if ((start_in == BF_LOOP_END) && (start_counter == 0)) {
             fseek(fp, 1, SEEK_CUR);
             break;
           }
@@ -76,26 +126,33 @@ int run() {
       }
       break;
 
-    case LOOP_END:
+    case BF_LOOP_END:
       if (*ptr != 0) {
         int close_counter = 0;
         do {
           fseek(fp, -2, SEEK_CUR);
           char end_in = fgetc(fp);
-          if (end_in == LOOP_END) {
+          if (end_in == BF_LOOP_END) {
             close_counter++;
-          } else if ((end_in == LOOP_START) && (close_counter > 0)) {
+          } else if ((end_in == BF_LOOP_START) && (close_counter > 0)) {
             close_counter--;
-          } else if ((end_in == LOOP_START) && (close_counter == 0)) {
+          } else if ((end_in == BF_LOOP_START) && (close_counter == 0)) {
             break;
           }
         } while(1 == 1);
-          fseek(fp, -1, SEEK_CUR);
+        fseek(fp, -1, SEEK_CUR);
       }
+      break;
+
+    case FOR_LOOP_START:
+      break;
+    case FOR_LOOP_END:
+      return for_loop();
       break;
     case EOF:
       return EOF;
   }
+
   return 0;
 }
 
@@ -114,8 +171,12 @@ int main(int argc, char **argv) {
     return -1;
   }
 
-  while (run() != EOF){continue;};
-  printf("\n");
+  int state = 0;
+
+  while (state != EOF){
+    state = run(fgetc(fp));
+  }
+
   fclose(fp);
   fp = NULL;
 
