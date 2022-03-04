7fa768ecea6ed9f14eaa420eeeff548a432ab6e2
Trevin Small <trevincub03@gmail.com>
2022-03-04 16:48:43 -0500

    Renaming project to bologna

--- bologna_interpreter.c
+++ bologna_interpreter.c
@@ -1,30 +1,6 @@
 #include <stdio.h>
 
-// Memory size of Brainfuck programs
-#define MEM_SIZE 30000
-
-// Vanilla Brainfucks commands
-#define PTR_LEFT_SHIFT     '<'
-#define PTR_RIGHT_SHIFT    '>'
-#define INCREMENT          '+'
-#define DECREMENT          '-'
-#define CHAR_IN            ','
-#define CHAR_OUT           '.'
-#define BRN_FCK_LOOP_START '['
-#define BRN_FCK_LOOP_END   ']'
-
-// Extended commands
-#define ZERO               '_'
-#define ABSOLUTE_SHIFT     ':'
-#define MULTIPLY           '*'
-#define DIVIDE             '/'
-#define PTR_VALUE          '#'
-#define FOR_LOOP_START     '{'
-#define FOR_LOOP_END       '}'
-#define EXIT               '~'
-
-int for_loop();
-int run(char);
+#include "bologna_interpreter.h"
 
 int mem[MEM_SIZE] = {0};
 int * ptr;
---
+++ bologna_interpreter.h
@@ -0,0 +1,26 @@
+// Memory size of Brainfuck programs
+#define MEM_SIZE 30000
+
+// Vanilla Brainfucks commands
+#define PTR_LEFT_SHIFT     '<'
+#define PTR_RIGHT_SHIFT    '>'
+#define INCREMENT          '+'
+#define DECREMENT          '-'
+#define CHAR_IN            ','
+#define CHAR_OUT           '.'
+#define BRN_FCK_LOOP_START '['
+#define BRN_FCK_LOOP_END   ']'
+
+// Extended commands
+#define ZERO               '_'
+#define ABSOLUTE_SHIFT     ':'
+#define MULTIPLY           '*'
+#define DIVIDE             '/'
+#define PTR_VALUE          '#'
+#define FOR_LOOP_START     '{'
+#define FOR_LOOP_END       '}'
+#define EXIT               '~'
+
+int read_args(int*);
+int for_loop();
+int run(char);
