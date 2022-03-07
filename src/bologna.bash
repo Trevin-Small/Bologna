d1f3124b2b17dd52665d204f27561bc91661b6f4
Trevin Small <trevincub03@gmail.com>
2022-03-07 02:21:54 -0500

    Improved bash file

--- src/bologna.bash
+++ src/bologna.bash
@@ -1,9 +1,14 @@
+if [ ! -f ./bologna_parser ]; then
 gcc ./bologna_parser.c -o ./bologna_parser
+fi
+
+if [ ! -f ./bologna_interpreter ]; then
 gcc ./bologna_interpreter.c -o ./bologna_interpreter
+fi
 
-touch temp.bf
+touch .temp_bologna
 
-./bologna_parser $1 temp.bf
-./bologna_interpreter temp.bf
+./bologna_parser $1 .temp_bologna
+./bologna_interpreter .temp_bologna
 
-rm temp.bf
+rm .temp_bologna
